e96835ddf6b5a15e9c69af5be16218f33069d11e
Diego Nuñez <diego.nm97@hotmail.com>
2019-02-20 17:58:40 -0500

    la buenaa

--- src/recorrerlista/RecorrerLista.java
+++ src/recorrerlista/RecorrerLista.java
@@ -27,64 +27,98 @@ public class RecorrerLista {
          LinkedList<Integer> LinkedListNumber = new LinkedList<>();
          
          ArrayList<Integer> ArrListNumber2 = new ArrayList<Integer>();
+         LinkedList<Integer> LinkedListNumber2 = new LinkedList<>();
+         
          ArrayList<Integer> ArrListNumber3 = new ArrayList<Integer>();
-           int cantidadElementos = 10000000; //   Cantidad de elentos dentro del arreglo
+         LinkedList<Integer> LinkedListNumber3 = new LinkedList<>();
+         
+         
+           int cantidadElementos = 800000; //   Cantidad de elentos dentro del arreglo
 //        for(int i = 0;i< cantidadElementos;i++){
 //             ArrListNumber.add(i, 0);  // Se establece 0 para cada unos de los elementos dentro del arreglo
 //        }
 //        int tamañoArreglo = ArrListNumber.size(); // tamaño del arreglo
         
-        double TiempoInicial = System.currentTimeMillis();
+        
         
                 
         
         //////////////////////////////////////// FOR ////////////////////7///////////////////////////////
+        double TiempoInicial = System.currentTimeMillis();
+        
         
         for(int i = 0;i< cantidadElementos;i++){
             int num = (int) (Math.random() * cantidadElementos);
              ArrListNumber.add(i, num);
         }
         
+        double TiempoFinalFOR = System.currentTimeMillis() - TiempoInicial;
+        System.out.println("Mediante ciclo for clasico tardo : " + TiempoFinalFOR + "ms");
+        
+         double TiempoInicial2 = System.currentTimeMillis();
+        
         for(int i = 0;i< cantidadElementos;i++){
             int num = (int) (Math.random() * cantidadElementos);
             LinkedListNumber.add(i, num);
         }
         
-        double TiempoFinalFOR = System.currentTimeMillis() - TiempoInicial;
-        System.out.println("Mediante ciclo for clasico tardo : " + TiempoFinalFOR + "ms");
-        
-        double TiempoFinalLink = System.currentTimeMillis() - TiempoInicial;
-        System.out.println("Mediante ciclo for en Linked tardo : " + TiempoFinalLink + "ms");
+        double TiempoFinalFORLink = System.currentTimeMillis() - TiempoInicial2;
+        System.out.println("Mediante ciclo for en Linked tardo : " + TiempoFinalFORLink + "ms");
        
         
+//////////////////////////////////////// FOR EACH ////////////////////////////////////////////////
+         double TiempoInicial3 = System.currentTimeMillis();
         
-       
-        //////////////////////////////////////// FOR EACH ////////////////////////////////////////////////
         ArrListNumber2.forEach((i) -> {
             int num = (int) (Math.random() * cantidadElementos);
-            ArrListNumber2.set(i, num);
+            ArrListNumber2.add(i, num);
         });
         
-        double TiempoFinalFOREACH = System.currentTimeMillis() - TiempoInicial - TiempoFinalFOR ;
-        System.out.println("Mediante ciclo for clasico tardo : " + TiempoFinalFOREACH + "ms");
+         double TiempoFinalFOREACH = System.currentTimeMillis() - TiempoInicial3;
+        System.out.println("Mediante ciclo for each tardo : " + TiempoFinalFOREACH + "ms");
+        
+          double TiempoInicial4 = System.currentTimeMillis();
+        
+        ArrListNumber2.forEach((i) -> {
+            int num = (int) (Math.random() * cantidadElementos);
+            LinkedListNumber2.add(i, num);
+        });
+        
+       
+        
+        double TiempoFinalFOREACHLink = System.currentTimeMillis() - TiempoInicial4;
+        System.out.println("Mediante ciclo for each en Linked tardo : " + TiempoFinalFOREACHLink + "ms");
      
         
         //////////////////////////////////////// ITERATOR /////////////////////////////////////////////////
         
+          double TiempoInicial5 = System.currentTimeMillis();
+        
         Iterator<Integer> i = ArrListNumber3.iterator();
         while(i.hasNext()){
            int num = (int) (Math.random() * cantidadElementos);
-            ArrListNumber3.set(i.next(), num);
+            ArrListNumber3.add(i.next(), num);
         }
         
-        double TiempoFinalI = System.currentTimeMillis() - TiempoInicial - TiempoFinalFOR - TiempoFinalFOREACH;
-        System.out.println("Mediante ciclo for clasico tardo : " + TiempoFinalI + "ms");
+        double TiempoFinalI = System.currentTimeMillis() - TiempoInicial5;
+        System.out.println("Mediante ciclo iterador tardo : " + TiempoFinalI + "ms");
+        
+        double TiempoInicial6 = System.currentTimeMillis();
+        
+        Iterator<Integer> i2 = LinkedListNumber3.iterator();
+        while(i2.hasNext()){
+           int num = (int) (Math.random() * cantidadElementos);
+            LinkedListNumber3.add(i2.next(), num);
+        }
+        
+        double TiempoFinalILink = System.currentTimeMillis() - TiempoInicial6;
+        System.out.println("Mediante ciclo iterador Linked tardo : " + TiempoFinalILink + "ms");
         
         /////////////////////////////////////////////////////////////////////////////////////////////////
      
         
         
-
+        System.out.println("===============================================================");
         if((TiempoFinalFOR < TiempoFinalFOREACH) && (TiempoFinalFOR < TiempoFinalI)){
         System.out.println("El menor tiempo fue de :" + TiempoFinalFOR + " / Metodo FOR ");
            } else if((TiempoFinalFOREACH < TiempoFinalFOR) && (TiempoFinalFOREACH < TiempoFinalI)){
@@ -92,9 +126,20 @@ public class RecorrerLista {
         } else{
          System.out.println("El menor tiempo fue de : " + TiempoFinalI + " / Metodo ITERATOR");
         }
-      
         
+        
+        System.out.println("===============================================================");
+        if((TiempoFinalFOREACHLink < TiempoFinalFOREACHLink) && (TiempoFinalFOREACHLink < TiempoFinalILink)){
+        System.out.println("El menor tiempo fue de :" + TiempoFinalFOREACHLink + " / Metodo FOR Linked");
+           } else if((TiempoFinalFOREACHLink < TiempoFinalFOREACHLink) && (TiempoFinalFOREACHLink < TiempoFinalILink)){
+         System.out.println("El menor tiempo fue de :" + TiempoFinalFOREACHLink + " / Metodo FOR EACH Linked");
+        } else{
+         System.out.println("El menor tiempo fue de : " + TiempoFinalILink + " / Metodo ITERATOR Linked");
+        }
       
+        long i66 = System.currentTimeMillis();       
+        System.out.println(i66);
+        
         
         
     }
